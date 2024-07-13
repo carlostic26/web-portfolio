42d49490494c10451c6d54e216983375a170e050
carlostic26 <carlospenaranda26@gmail.com>
2024-07-13 10:10:40 -0500

    terminar todo de data_dev

--- lib/config/data_dev.dart
+++ lib/config/data_dev.dart
@@ -1,6 +1,20 @@
 class Dev {
   static String name = 'Carlos Peñaranda';
-  static String email = '@gmail.com';
+  static String email = 'carlospenaranda26@gmail.com';
   static String urlLinkedin = '';
   static String urlGithub = '';
+
+  //TODO: Crear mapa de proyectosPlayStore y/o repositorio github,
+  //el mapa clave: valor
+  //en clave el nombre del proyecto, en valor el link
+
+  /*
+
+final mapProyectosPlayStore = {
+    'WebPortfolio': 'https://github.com/portfolio',
+    'TaskFlow': 'https://github.com/portfolio',
+  };
+
+
+  */
 }
