87953ac5ab77ac16322af83b986d317c692efd51
Elias Montilla <eliasbest634@gmail.com>
2024-07-18 17:19:50 -0500

    datos mapeados

--- lib/domain/project_utils.dart
+++ lib/domain/project_utils.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class ProjectUtils {
   final String image;
   final String title;
@@ -16,6 +18,26 @@ class ProjectUtils {
     this.webLink,
     this.githubLink,
   });
+
+     factory ProjectUtils.fromJson(Map<String, dynamic> json) => ProjectUtils(
+        subtitle: json["subtitle"],
+        image: json["image"],
+        title: json["title"],
+        androidLink: json["androidLink"],
+        iosLink: json["iosLink"],
+        webLink: json["webLink"],
+        githubLink: json["githubLink"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "subtitle": subtitle,
+        "image": image,
+        "title": title,
+        "androidLink": androidLink,
+        "iosLink": iosLink,
+        "webLink": webLink,
+        "githubLink": githubLink,
+    };
 }
 
 //HOBBY PROJECTS
@@ -31,3 +53,10 @@ List<ProjectUtils> workProjectUtils = [
   ProjectUtils(image: 'image', title: 'title', subtitle: ''),
   ProjectUtils(image: 'image', title: 'title', subtitle: ''),
 ];
+
+//METHODS TO GET LIST
+
+List<ProjectUtils> projectUtilsFromJson(String str) => List<ProjectUtils>.from(json.decode(str).map((x) => ProjectUtils.fromJson(x)));
+
+String projectUtilsToJson(List<ProjectUtils> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
