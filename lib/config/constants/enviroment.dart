64d11372a260af52c98ed320c0604a2702a563f8
juandpt03 <juandpt03@gmail.com>
2024-07-20 17:58:59 -0500

    fix: space in environment variable name fixed

--- lib/config/constants/enviroment.dart
+++ lib/config/constants/enviroment.dart
@@ -2,5 +2,5 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Enviroment {
   static String envSupabaseDbUrl = dotenv.env['envSupabaseDbUrl'] ?? 'no-url';
-  static String apiKeySupabase = dotenv.env['apiKeySupabase '] ?? 'no-key ';
+  static String apiKeySupabase = dotenv.env['apiKeySupabase'] ?? 'no-key ';
 }
